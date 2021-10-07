8d047bda5ae42490fbff035775ff10545b6a5a57
Adithya S Shenoy <you@example.com>
2021-10-07 17:45:35 +0530

    Added:Easy-Note app

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.use(express.json())
+
+require('./app/routes/note.routes.js')(app);
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+const server = app.listen(3000, () => {
+   const host = server.address().address
+   const port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
